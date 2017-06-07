68d20eceb56fd87a4416e5e79cd10482aa75d519
Vladimir Ignatov <vladimir.ignatov@childrens.harvard.edu>
2017-06-07 17:15:40 -0400

    Updating sample apps...

--- authorize/app.js
+++ authorize/app.js
@@ -1,159 +1,152 @@
 function urlParam(p) {
-  var query = location.search.substr(1);
-  var data = query.split("&");
-  var result = [];
+    var query  = location.search.replace(/^\?/, "");
+    var data   = query.split("&");
+    var result = [];
+    var i, item;
 
-  for(var i=0; i<data.length; i++) {
-    var item = data[i].split("=");
-    if (item[0] === p) {
-      var res = item[1].replace(/\+/g, '%20');
-      result.push(decodeURIComponent(res));
+    for (i = 0; i < data.length; i++) {
+        item = data[i].split("=");
+        if (item[0] === p) {
+            return decodeURIComponent(item[1].replace(/\+/g, '%20'));
+        }
     }
-  }
 
-  if (result.length === 0){
     return null;
-  }
-  return result[0];
 }
 
-function getRedirectURI () {
-    return (window.location.protocol + "//" + window.location.host + window.location.pathname).match(/(.*\/)[^\/]*/)[1];
+function getRedirectURI() {
+    return (location.protocol + "//" + location.host + location.pathname)
+        .match(/(.*\/)[^\/]*/)[1];
 }
 
-function refreshApp () {
-    window.location.href = getRedirectURI();
+function refreshApp() {
+    location.href = getRedirectURI();
 }
 
-function initialize (settings) {
-
+function initialize(settings) {
     setSettings({
-        client_id: settings.client_id,
-        secret: settings.secret,
-        scope: settings.scope + " launch",
-        launch_id: urlParam("launch"),
+        client_id     : settings.client_id,
+        secret        : settings.secret,
+        scope         : settings.scope + " launch",
+        launch_id     : urlParam("launch"),
         api_server_uri: urlParam("iss")
     });
     clearAuthToken();
     refreshApp();
 }
 
-function completeAuth () {
-    $.when(fetchToken()).then(function () {
-        refreshApp();
-    });
+function completeAuth() {
+    return fetchToken().then(refreshApp);
 }
 
-function writeData (key, data) {
+function writeData(key, data) {
     sessionStorage[key] = JSON.stringify(data);
 }
 
-function readData (key) {
+function readData(key) {
     var data = sessionStorage[key];
     if (data) {
-        return JSON.parse(sessionStorage[key]);
-    } else {
-        return data;
+        return JSON.parse(data);
     }
+    return data;
 }
 
-function clearData (key) {
+function clearData(key) {
     delete sessionStorage[key];
 }
 
-function getAuthToken () {
+function getAuthToken() {
     return readData("auth-token");
 }
 
-function setAuthToken (data) {
-    writeData ("auth-token", data);
+function setAuthToken(data) {
+    writeData("auth-token", data);
 }
 
-function clearAuthToken () {
+function clearAuthToken() {
     clearData("auth-token");
 }
 
-function getSettings () {
+function getSettings() {
     return readData("app-settings");
 }
 
-function setSettings (data) {
-    writeData ("app-settings", data);
+function setSettings(data) {
+    writeData("app-settings", data);
 }
 
-function getSession (key) {
+function getSession(key) {
     return readData(key);
 }
 
-function setSession (data) {
-    var key = Math.round(Math.random()*100000000).toString();
-    writeData (key, data);
+function setSession(data) {
+    var key = Math.round(Math.random() * 100000000).toString();
+    writeData(key, data);
     return key;
 }
 
-function hasAuthToken () {
-    return getAuthToken () !== undefined;
+function hasAuthToken() {
+    return getAuthToken() !== undefined;
 }
 
-function fetchToken () {
-    var differed = $.Deferred();
-    var settings = getSettings ();
-    var state = urlParam("state");
-    var code = urlParam("code");
-    var params = getSession (state);
+function fetchToken() {
+    var settings = getSettings();
+    var state    = urlParam("state");
+    var code     = urlParam("code");
+    var params   = getSession(state);
     var data = {
-            code: code,
-            grant_type: 'authorization_code',
-            redirect_uri: getRedirectURI()
-        };
+        code        : code,
+        grant_type  : 'authorization_code',
+        redirect_uri: getRedirectURI()
+    };
     var options;
-        
+
     if (!settings.secret) {
         data['client_id'] = settings.client_id;
     }
-    
+
     options = {
-        url: params.token_uri,
+        url : params.token_uri,
         type: 'POST',
         data: data
     };
-    
+
     if (settings.secret) {
-        options['headers'] = {'Authorization': 'Basic ' + btoa(settings.client_id + ':' + settings.secret)};
+        options['headers'] = {
+            'Authorization': 'Basic ' + btoa(settings.client_id + ':' + settings.secret)
+        };
     }
-    
-    $.ajax(options).done(function(res){
+
+    return $.ajax(options).then(function(res) {
         setAuthToken({
-            patient_id: res.patient,
+            patient_id : res.patient,
             access_token: res.access_token
         });
-        differed.resolve ();
     });
-    
-    return differed.promise();
 }
 
-function authorize () {
-    var settings = getSettings ();
-    
+function authorize() {
+    var settings = getSettings();
+
     $.get(
-        settings.api_server_uri+"/metadata",
-        function(r){
+        settings.api_server_uri + "/metadata",
+        function(r) {
             var authorize_uri = null;
-            var token_uri = null;
+            var token_uri     = null;
 
             var smartExtension = r.rest[0].security.extension.filter(function (e) {
                return (e.url === "http://fhir-registry.smarthealthit.org/StructureDefinition/oauth-uris");
             });
 
-            smartExtension[0].extension.forEach(function(arg, index, array){
-              if (arg.url === "authorize") {
-                authorize_uri = arg.valueUri;
-              } else if (arg.url === "token") {
-                token_uri = arg.valueUri;
-              }
+            smartExtension[0].extension.forEach(function(arg, index, array) {
+                if (arg.url === "authorize") {
+                    authorize_uri = arg.valueUri;
+                }
+                else if (arg.url === "token") {
+                    token_uri = arg.valueUri;
+                }
             });
-            
+
             var state = setSession({
                 token_uri: token_uri
             });
@@ -166,36 +159,32 @@ function authorize () {
                 "aud=" + encodeURIComponent(settings.api_server_uri) + "&" +
                 "launch=" + settings.launch_id + "&" +
                 "state=" + state;
-            
-            window.location.href = redirect_to;
+
+            location.href = redirect_to;
         },
         "json"
     );
 }
 
-function getPatientName () {
-    var ret = $.Deferred();
+function getPatientName() {
     var api_server_uri = getSettings().api_server_uri;
-    var patient_id = getAuthToken().patient_id;
-    var access_token = getAuthToken().access_token;
-    var url = api_server_uri + '/Patient/' + patient_id;
-      
+    var patient_id     = getAuthToken().patient_id;
+    var access_token   = getAuthToken().access_token;
+    var url            = api_server_uri + '/Patient/' + patient_id;
 
-    $.ajax({
-        type: 'GET',
-        url: url,
+    return $.ajax({
+        type    : 'GET',
+        url     : url,
         dataType: 'json',
-        headers: {
+        headers : {
             'Authorization': "Bearer " + access_token
         }
-    })
-    .done(function(pt){
-        var name = pt.name[0].given.join(" ") +" "+ pt.name[0].family.join(" ");
-        ret.resolve(name);
-    })
-    .fail(function(){
-        ret.reject("Could not fetch " + url);
-    });
-
-    return ret.promise();
+    }).then(
+        function(pt) {
+            return pt.name[0].given.join(" ") + " " + pt.name[0].family.join(" ");
+        },
+        function() {
+            return "Could not fetch " + url;
+        }
+    );
 }
