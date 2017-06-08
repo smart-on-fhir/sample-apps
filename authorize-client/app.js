2a40f22bf8f17c774064c4c9396611297528a872
Vladimir Ignatov <vladimir.ignatov@childrens.harvard.edu>
2017-06-08 15:43:11 -0400

    Added the authorize-client app

--- authorize-client/app.js
+++ authorize-client/app.js
@@ -1,136 +1,134 @@
 function urlParam(p) {
-  var query = location.search.substr(1);
-  var data = query.split("&");
-  var result = [];
+    var query  = location.search.replace(/^\?/, "");
+    var data   = query.split("&");
+    var result = [];
+    var i, item;
 
-  for(var i=0; i<data.length; i++) {
-    var item = data[i].split("=");
-    if (item[0] === p) {
-      result.push(decodeURIComponent(item[1]));
+    for (i = 0; i < data.length; i++) {
+        item = data[i].split("=");
+        if (item[0] === p) {
+            return decodeURIComponent(item[1].replace(/\+/g, '%20'));
+        }
     }
-  }
 
-  if (result.length === 0){
     return null;
-  }
-  return result[0];
 }
 
-function getRedirectURI () {
-    return (window.location.protocol + "//" + window.location.host + window.location.pathname).match(/(.*\/)[^\/]*/)[1];
+function getRedirectURI() {
+    return (location.protocol + "//" + location.host + location.pathname)
+        .match(/(.*\/)[^\/]*/)[1];
 }
 
-function refreshApp () {
-    window.location.href = getRedirectURI();
+function refreshApp() {
+    location.href = getRedirectURI();
 }
 
-function initialize (settings) {
-
+function initialize(settings) {
     setSettings({
-        client_id: settings.client_id,
-        scope: settings.scope + " launch",
-        launch_id: urlParam("launch"),
+        client_id     : settings.client_id,
+        secret        : settings.secret,
+        scope         : settings.scope + " launch",
+        launch_id     : urlParam("launch"),
         api_server_uri: urlParam("iss")
     });
     clearAuthToken();
     refreshApp();
 }
 
-function completeAuth () {
-    FHIR.oauth2.ready(function(){
-        refreshApp();
-    });
+function completeAuth() {
+    FHIR.oauth2.ready(refreshApp);
 }
 
 function writeData (key, data) {
     sessionStorage[key] = JSON.stringify(data);
 }
 
-function readData (key) {
+function readData(key) {
     var data = sessionStorage[key];
     if (data) {
-        return JSON.parse(sessionStorage[key]);
-    } else {
-        return data;
+        return JSON.parse(data);
     }
+    return data;
 }
 
-function getSettings () {
+function getSettings() {
     return readData("app-settings");
 }
 
-function setSettings (data) {
-    writeData ("app-settings", data);
+function setSettings(data) {
+    writeData("app-settings", data);
 }
 
-function hasAuthToken () {
+function hasAuthToken() {
     return sessionStorage.tokenResponse !== undefined;
 }
 
-function clearAuthToken () {
+function clearAuthToken() {
     delete sessionStorage.tokenResponse;
 }
 
-function authorize () {
-    var settings = getSettings ();
-    
+function getHumanName(name) {
+    return name.given.join(" ") + " " + name.family.join(" ");
+}
+
+function authorize() {
+    var settings = getSettings();
+
     FHIR.oauth2.authorize({
         "client": {
             "client_id": settings.client_id,
-            "scope":  settings.scope,
-            "launch": settings.launch_id
+            "scope"    : settings.scope,
+            "launch"   : settings.launch_id
         },
         "server": settings.api_server_uri
     });
 }
 
-function getPatientName () {
+function fetchPatientName () {
     var ret = $.Deferred();
-    
-    FHIR.oauth2.ready(function(smart){
+
+    FHIR.oauth2.ready(function(smart) {
         var patient = smart.patient;
-
-        patient.read()
-          .then(function(pt) {
-            var name = pt.name[0].given.join(" ") +" "+ pt.name[0].family.join(" ");
-            ret.resolve(name)
-          })
-          .fail(function() {
+        patient.read().then(function(pt) {
+            ret.resolve(getHumanName(pt.name[0]));
+        }).fail(function() {
             ret.reject("Could not fetch patient name");
-          });
-
+        });
     });
 
     return ret.promise();
 }
 
-function getUserName () {
+function getUserName() {
     var ret = $.Deferred();
-    
+
     FHIR.oauth2.ready(function(smart){
         var user = smart.user;
-
-         $.when(user.read())
-          .then(function(pt) {
+        
+        // smart.userId = "Patient/" + smart.userId
+        $.when(user.read())
+        .then(function(pt) {
             if (pt) {
                 var name;
                 if (pt.resourceType === "Practitioner" || pt.resourceType === "RelatedPerson") {
-                    name = pt.name.given.join(" ") +" "+ pt.name.family.join(" ");
-                    ret.resolve(name);
-                } else if (pt.resourceType === "Patient") {
-                    name = pt.name[0].given.join(" ") +" "+ pt.name[0].family.join(" ");
-                    ret.resolve(name);
-                } else {
+                    ret.resolve(getHumanName(pt.name));
+                }
+                else if (pt.resourceType === "Patient") {
+                    ret.resolve(getHumanName(pt.name[0]));
+                }
+                else {
                     ret.reject("Could not fetch user name");
                 }
-            } else {
+            }
+            else {
                 ret.resolve(pt);
             }
-          })
-          .fail(function() {
-            ret.reject("Could not fetch user name");
-          });
-
+        })
+        .fail(function(error) {
+            window.SMART = smart
+            console.log(smart)
+            ret.reject("Could not fetch user name: " + error);
+        });
     });
 
     return ret.promise();
